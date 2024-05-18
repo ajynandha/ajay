1df00ffcf9b4fb567714c2edb91ce17655e92893
Ajaynandha0 <ajaynadha01@gnail.com>
2024-05-18 18:28:20 +0530

    js

---
+++ form/input.js
@@ -0,0 +1,8 @@
+function check(form){
+    if(form.username.value == "Ajay kumar" && form.userpassword.value == "nandha"){
+        alert("login successfully")
+    }
+    else{
+        alert("incorrect password or incorrect username")
+    }
+}
