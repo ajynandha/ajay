d4a266124388f358358cbc7825c93a8c8951d201
Ajaynandha0 <ajaynadha01@gnail.com>
2024-07-06 16:29:01 +0530

    js

---
+++ form/js.js
@@ -0,0 +1,12 @@
+
+let profileDetails = {
+    name: "Ajay Kumar",
+    age: 25,
+    image:
+      "https://news-cdn.softpedia.com/images/news2/microsoft-founder-bill-gates-again-the-richest-man-in-the-world-514123-2.jpg",
+  };
+  
+  document.getElementById("photo").src = profileDetails.image;
+  document.getElementById("name").textContent = profileDetails.name;
+  document.getElementById("age").textContent = profileDetails.age;
+  
